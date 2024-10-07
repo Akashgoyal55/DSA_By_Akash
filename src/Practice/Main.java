3ce635b5d5f21e1638816494996bcb186c329cfe
Akashgoyal55 <agoyal1@sfsu.edu>
2024-10-06 20:41:04 -0700

    test

---
+++ src/Practice/Main.java
@@ -0,0 +1,17 @@
+//binary search - an algorithm to find the target element in an array, it's take a time complexity of o(log n)
+package Practice;
+public class Main{
+    public static void main(String[] args) {
+
+    }
+
+    static int target(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+
+        }
+    }
+}
+
