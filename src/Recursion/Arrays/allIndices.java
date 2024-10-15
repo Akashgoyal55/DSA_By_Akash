fae0cbede5cdfc0c36fc6d252b1eab0d1e24ac94
Akashgoyal55 <agoyal1@sfsu.edu>
2024-10-14 23:25:01 -0700

    test

---
+++ src/Recursion/Arrays/allIndices.java
@@ -0,0 +1,10 @@
+package Recursion.Arrays;
+/*
+* Returning an arrayList, by passing the answer list inside the parameter
+* Note - Things that we add inside the parameter, that will be passed to function call
+*        Things that we add inside the function body, it will only be available for that function call
+*
+ */
+public class allIndices {
+
+}
