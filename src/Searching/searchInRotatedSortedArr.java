ed79e28adc81d7efe906edacfa425d82be4dfc96
Akashgoyal55 <agoyal1@sfsu.edu>
2025-01-11 15:57:54 -0800

    test

---
+++ src/Searching/searchInRotatedSortedArr.java
@@ -0,0 +1,37 @@
+package Searching;
+//  find the peak/pivot then search for target in left and then right
+public class searchInRotatedSortedArr {
+    public static void main(String[] args) {
+
+    }
+    static int rotetedArray(int[] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            if(nums[start]<nums[mid]){
+                //left side is sorted
+                if(nums[start]<=target && target<nums[mid]){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }else{
+                //right side is sorted
+                if(nums[mid]<target && target<nums[end]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    }
+}
